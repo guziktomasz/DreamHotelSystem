6cd17fa5a74ff460709a166441e8bb80005cde42
Tomek <guziktomasz@o2.pl>
2022-08-30 18:19:52 +0200

    start git

---
+++ src/main/java/pl/sda/dreamhotelsystem/DreamHotelSystemApplication.java
@@ -0,0 +1,15 @@
+package pl.sda.dreamhotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamHotelSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DreamHotelSystemApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/pl/sda/dreamhotelsystem/DreamHotelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.dreamhotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamHotelSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
