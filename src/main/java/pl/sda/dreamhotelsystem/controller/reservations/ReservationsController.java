3f5ca2f57a05372d4bd11017670ca7c22a6bd90a
DurkaKlaudia <durka.klaudia@interia.eu>
2022-09-14 18:51:12 +0200

    reservation - localHost:4200 connection

--- src/main/java/pl/sda/dreamhotelsystem/controller/reservations/ReservationsController.java
+++ src/main/java/pl/sda/dreamhotelsystem/controller/reservations/ReservationsController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/reservations")
 public class ReservationsController {
 
