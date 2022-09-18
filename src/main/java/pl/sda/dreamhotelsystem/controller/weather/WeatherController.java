0397444eef85a1a1169e62cb8aa815b67d659535
Tomek <guziktomasz@o2.pl>
2022-09-18 19:33:23 +0200

    WetherControlor not working

--- src/main/java/pl/sda/dreamhotelsystem/controller/weather/WeatherController.java
+++ src/main/java/pl/sda/dreamhotelsystem/controller/weather/WeatherController.java
@@ -3,17 +3,23 @@ package pl.sda.dreamhotelsystem.controller.weather;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.sda.dreamhotelsystem.domain.Weather;
 import pl.sda.dreamhotelsystem.dto.weathers.WeatherDto;
 import pl.sda.dreamhotelsystem.service.weather.WeatherService;
 
 import java.util.Optional;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "/weatherToday")
 @AllArgsConstructor
 public class WeatherController {
     private WeatherService weatherService;
-    @Bean
+    @GetMapping
     public Weather getActuallyWeatherInCelsius(){
         float celsiusTemperature = (weatherService.getTemperature().get().getTemperatureValue() - 32) * 5/9;
         celsiusTemperature = Math.round(celsiusTemperature);
